cd850e6b33ecbc7b55c9eecae41993b74e9ef498
Trevor <me@trev.app>
2024-04-27 13:24:26 -0400

    Add cordl-less hooking macros

--- fujsu/include/fujsu.h
+++ fujsu/include/fujsu.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <android/log.h>
+#include <dlfcn.h>
+#include <cstdint>
 
 namespace fujsu {
     extern "C"
@@ -36,7 +38,7 @@ namespace fujsu {
     {                                                                                                                      \
         using funcType = retval (*)(__VA_ARGS__);                                                                          \
         constexpr static const char *name() { return #name_; }                                                             \
-        static const MethodInfo *getInfo() { return il2cpp_utils::il2cpp_type_check::MetadataGetter<mPtr>::methodInfo(); } \
+        static const MethodInfo *getInfo() { return il2cpp_utils::il2cpp_type_check::MetadataGetter<mPtr>::get(); } \
         static funcType *trampoline() { return &name_; }                                                                   \
         static inline retval (*name_)(__VA_ARGS__) = nullptr;                                                              \
         static funcType hook() { return &hook_##name_; }                                                                   \
@@ -51,6 +53,34 @@ void InstallHook() {
     auto info = T::getInfo();
     auto addr = (void *) info->methodPointer;
     void *trampoline = fujsu::hook(addr, (void *) T::hook());
+    (*(void **) T::trampoline()) = trampoline;
+}
+#else
+#define CREATE_HOOK(name_, retval, ...)                                                                              \
+    struct Hook_##name_                                                                                                    \
+    {                                                                                                                      \
+        using funcType = retval (*)(__VA_ARGS__);                                                                          \
+        constexpr static const char *name() { return #name_; }                                                             \
+        static funcType *trampoline() { return &name_; }                                                                   \
+        static inline retval (*name_)(__VA_ARGS__) = nullptr;                                                              \
+        static funcType hook() { return &hook_##name_; }                                                                   \
+        static retval hook_##name_(__VA_ARGS__);                                                                           \
+    };                                                                                                                     \
+    retval Hook_##name_::hook_##name_(__VA_ARGS__)
+
+#define INSTALL_HOOK_WITH_ADDR(name, addr) InstallHookWithAddr<Hook_##name>((void*)addr);
+
+template<typename T>
+void InstallHookWithAddr(void* addr) {
+    // TODO: this is awful
+    void* mod = dlopen("libil2cpp.so", RTLD_LAZY);
+    Dl_info info;
+    void* initPtr = dlsym(mod, "il2cpp_init");
+    dladdr(initPtr, &info);
+    char* addrChar = reinterpret_cast<char*>(info.dli_fbase);
+    dlclose(mod);
+
+    void *trampoline = fujsu::hook((void*)(addrChar + reinterpret_cast<std::uintptr_t>(addr)), (void *) T::hook());
     (*(void **) T::trampoline()) = trampoline;
 }
 #endif
